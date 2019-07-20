ea2303ea65b681b19c1b5a0296c3d49da9eb60e6
Hearty Oh <heartyoh@hatiolab.com>
2019-07-20 21:27:16 +0900

    v0.2.2

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -1,6 +1,6 @@
 import { html } from 'lit-html'
 import { store } from '@things-factory/shell'
-import { APPEND_ASIDEBAR, TOOL_POSITION, toggleOverlay } from '@things-factory/layout-base'
+import { appendViewpart, VIEWPART_POSITION, TOOL_POSITION, toggleOverlay } from '@things-factory/layout-base'
 import { APPEND_APP_TOOL } from '@things-factory/apptool-base'
 
 import '@material/mwc-icon'
@@ -8,16 +8,16 @@ import '@material/mwc-icon'
 export default function bootstrap() {
   import('./layout/more-panel')
 
-  store.dispatch({
-    type: APPEND_ASIDEBAR,
+  appendViewpart({
     name: 'more',
-    asidebar: {
+    viewpart: {
       show: false,
       hovering: 'edge',
       template: html`
         <more-panel></more-panel>
       `
-    }
+    },
+    position: VIEWPART_POSITION.ASIDEBAR
   })
 
   store.dispatch({
